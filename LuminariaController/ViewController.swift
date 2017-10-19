1de7c1388a672ddd3c0b312d3865b68ae4ae8d17
JShanks <jhojan.guillermo@tecsup.edu.pe>
2017-10-18 20:11:53 -0500

    Integrador

--- LuminariaController/ViewController.swift
+++ LuminariaController/ViewController.swift
@@ -1,25 +1,63 @@
 //
 //  ViewController.swift
-//  LuminariaController
+//  ArduinoController
 //
-//  Created by JShanks on 10/18/17.
-//  Copyright © 2017 Tecsup. All rights reserved.
+//  Created by jatin patel on 10/06/16.
+//  Copyright © 2016 JatinPatel. All rights reserved.
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,StreamDelegate,UIWebViewDelegate {
+    
+    let webView = UIWebView()
+    
+    var inputStream:  InputStream!
+    var outputStream:  OutputStream!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
+        initCommunication()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initCommunication() {
+        var readStream:  Unmanaged<CFReadStream>?
+        var writeStream: Unmanaged<CFWriteStream>?
+        
+        CFStreamCreatePairWithSocketToHost(nil, "192.168.4.1" as CFString!, 80, &readStream, &writeStream)
+        
+        self.inputStream = readStream!.takeRetainedValue()
+        self.outputStream = writeStream!.takeRetainedValue()
+        
+        self.inputStream.delegate = self
+        self.outputStream.delegate = self
+        
+        self.inputStream.schedule(in: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+        self.outputStream.schedule(in: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+        
+        self.inputStream.open()
+        self.outputStream.open()
+        
     }
-
-
+    
+    func outputStringToServer(_ str: String) {
+        
+        let data: Data = str.data(using: String.Encoding.utf8)!
+        outputStream.write((data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count), maxLength: data.count)
+    }
+    
+    @IBAction func tapped_6(_ sender: AnyObject) {
+        outputStringToServer("pin=6")
+    }
+    
+    @IBAction func tapped_7(_ sender: AnyObject) {
+        outputStringToServer("pin=7")
+    }
+    
+    //
 }
+
+
 
---
+++ Pods/Target Support Files/Pods-LuminariaController/Pods-LuminariaController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LuminariaController : NSObject
+@end
+@implementation PodsDummy_Pods_LuminariaController
+@end
