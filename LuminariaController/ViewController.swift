460560a0dc73b6abdd17c00f50d9d5106696f266
Mac Tecsup <martin.chavez@tecsup.edu.pe>
2017-12-14 12:59:10 -0500

    Integracion con microfono

--- LuminariaController/ViewController.swift
+++ LuminariaController/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Speech
 import Alamofire
 
 class ViewController: UIViewController,StreamDelegate,UIWebViewDelegate {
@@ -20,6 +21,14 @@ class ViewController: UIViewController,StreamDelegate,UIWebViewDelegate {
     @IBOutlet weak var foco7: UIButton!
     @IBOutlet weak var foco8: UIButton!
     
+    @IBOutlet weak var micButton: UIButton!
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    var comandos: [String] = ["prender", "apagar"]
+    var idFocos: [String] = ["uno","dos","tres","cuatro","cinco","seis","siete","ocho"]
+    var microfonoEncendido = false
     
     let webView = UIWebView()
     
@@ -55,6 +64,95 @@ class ViewController: UIViewController,StreamDelegate,UIWebViewDelegate {
         
         let data: Data = str.data(using: String.Encoding.utf8)!
         outputStream.write((data as NSData).bytes.bindMemory(to: UInt8.self, capacity: data.count), maxLength: data.count)
+    }
+    
+    @IBAction func micButtonPressed(_ sender: Any) {
+        if microfonoEncendido {
+            stopRecording()
+        }else{
+            self.microfonoEncendido = true
+            self.micButton.setImage(#imageLiteral(resourceName: "microOn"), for: .normal)
+            SFSpeechRecognizer.requestAuthorization {
+                [unowned self] (authStatus) in
+                switch authStatus {
+                case .authorized:
+                    do {
+                        try self.startRecording()
+                    } catch let error {
+                        print("There was a problem starting recording: \(error.localizedDescription)")
+                    }
+                case .denied:
+                    print("Speech recognition authorization denied")
+                case .restricted:
+                    print("Not available on this device")
+                case .notDetermined:
+                    print("Not determined")
+                }
+            }
+        }
+    }
+    
+    func startRecording() throws{
+        print("startRecording")
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        
+        // 2
+        node.removeTap(onBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024,
+                        format: recordingFormat) { [unowned self]
+                            (buffer, _) in
+                            self.request.append(buffer)
+        }
+        
+        // 3
+        audioEngine.prepare()
+        try audioEngine.start()
+        recognitionTask = speechRecognizer?.recognitionTask(with: request) {
+            [unowned self]
+            (result, _) in
+            if let transcription = result?.bestTranscription {
+                print(transcription.formattedString)
+                for comando in self.comandos {
+                    if transcription.formattedString.lowercased().range(of:comando) != nil {
+                        for idFoco in self.idFocos{
+                            if transcription.formattedString.lowercased().range(of:idFoco) != nil {
+                                self.stopRecording()
+                                self.sendServer(idFoco: idFoco)
+                                print(idFoco)
+                            }
+                        }
+                        print(comando)
+                    }
+                }
+            }
+        }
+    }
+    
+    func stopRecording() {
+        print("stopRecording")
+        audioEngine.stop()
+        request.endAudio()
+        recognitionTask?.cancel()
+        microfonoEncendido = false
+        micButton.setImage(#imageLiteral(resourceName: "microOff"), for: .normal)
+    }
+    
+    func sendServer(idFoco: String){
+        //del 2 al 9
+        var nFoco = idFocos.index(of: idFoco)!
+        nFoco = nFoco+2
+        //outputStringToServer("pin=\(nFoco)")
+        let alertaVC = UIAlertController(title: "Exito!", message: "Ya lo prendí :)", preferredStyle: .alert)
+        let okAccion = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertaVC.addAction(okAccion)
+        self.present(alertaVC, animated: true, completion: nil)
+        //Alamofire.request("http://192.168.11.15/?pin=\(nFoco)").responseJSON{ response in
+          //  print("Result : \(response.result)")
+            //if let json = response.result.value {
+            //  print("JSON: \(json)")
+            //}
+        //}
     }
     
     @IBAction func foc1(_ sender: Any) {
